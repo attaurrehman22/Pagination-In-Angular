28b889968dbb6f27a59cb67616adb14f3cb8239f
Atta <attaurrehmanshah22@gmail.com>
2023-10-13 11:18:36 +0500

    final comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'paginationinangular';
+
+  data=[
+    {name:"raman",mobile:9623134},
+    {name:"Nokia",mobile:9623134},
+    {name:"Huwai",mobile:9623134},
+    {name:"Infinix",mobile:9623134},
+    {name:"Infinix 1",mobile:9623134},
+    {name:"Infinix 2",mobile:9623134},
+    {name:"Infinix 3",mobile:9623134},
+    {name:"Infinix 4",mobile:9623134},
+    {name:"Infinix 5",mobile:9623134},
+    {name:"Infinix 7",mobile:9623134},
+    {name:"Infinix 8",mobile:9623134},
+    {name:"Infinix 9",mobile:9623134},
+    {name:"Infinix 10",mobile:9623134},
+    {name:"Infinix 11",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+    {name:"raman",mobile:9623134},
+  ]
+
+  psize=5;
+  currentpage=1;
 }
